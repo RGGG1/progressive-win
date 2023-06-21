e5c0bc2a3000abe3137884013a067c9d36a0854b
RGGG1 <>
2023-06-21 17:56:57 +0000

    corrected typos

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ import random
 Options = [1,2,3,4,5,6,7,8,9,10,11]
 
 """
-   Options eliminaged - shows all the numbers already selected by the user
+   Options eliminated - shows all the numbers already selected by the user
 
 """
 
